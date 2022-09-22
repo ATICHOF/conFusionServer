2b17c42092aa2e5b2054d645b46c5f24ef141cee
ATICHOF <sof.atiche@gmail.com>
2022-09-22 16:57:23 +0100

    facebook authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "811002299937440",
+    clientSecret: "79c17153161249eacff1488d0e7ace5e",
+  },
 };
